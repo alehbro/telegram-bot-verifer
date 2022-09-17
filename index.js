0817331a57d6bfd0b448c9a2d7a7f3d1b8cd5f4c
Oleg <snaik.men@gmail.com>
2022-09-17 20:02:23 +0300

    changed puppeteer config

--- index.js
+++ index.js
@@ -33,25 +33,13 @@ app.get('/', async (req, res) => {
 app.get('/get-cards', async (req, res) => {
     var URL = 'https://ksp.co.il/web/cat/35..1044..15848';
 
-    const browser = await puppeteer.launch({
-        'args' : [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-        ],
-        headless: true
-    });
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'], headless: true});
 
     const page = await browser.newPage();
 
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
 
     await page.emulateTimezone('Asia/Jerusalem');
-
-    // get the User Agent on the context of Puppeteer
-    const userAgent = await page.evaluate(() => navigator.userAgent );
-
-    // If everything correct then no 'HeadlessChrome' sub string on userAgent
-    console.log(userAgent);
 
     await page.goto(URL);
 
